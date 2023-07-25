cb713b7c76fec5a921ca8656dea597faaa5b609a
Abduxalil Voxidjonov <99504511+AbduxalilVoxidjonov@users.noreply.github.com>
2023-07-26 01:41:35 +0500

    Update MainActivity.kt

--- app/src/main/java/com/example/movieapp/MainActivity.kt
+++ app/src/main/java/com/example/movieapp/MainActivity.kt
@@ -7,25 +7,20 @@ import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.example.movieapp.databinding.ActivityAddMovieBinding
+import com.example.movieapp.databinding.ActivityEditMovieBinding
 import com.example.movieapp.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
     private  val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
-    private lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         var array = mutableListOf<Malumot>()
-        sharedPreferences = getSharedPreferences("User", MODE_PRIVATE)
-        val name = sharedPreferences.getString("name", "")
-        val about = sharedPreferences.getString("about", "")
-        val time = sharedPreferences.getString("time", "")
-        sharedPreferences.edit().clear().apply()
-        array.add(Malumot(name.toString(),about.toString(), time.toString()))
 
         val customAdapter = MyCustomAdapter(array)
         binding.recycleView.adapter = customAdapter
@@ -33,7 +28,30 @@ class MainActivity : AppCompatActivity() {
 
         customAdapter.setListener(object : RecycleViewEditRemove {
             override fun editItem(position: Int) {
+                val dialogCustom = ActivityEditMovieBinding.inflate(layoutInflater,null,false)
+                val custom = ActivityAddMovieBinding.inflate(layoutInflater,null,false)
+                dialogCustom.nameMovie.setText(array[position].name)
+                dialogCustom.aboutMovei.setText(array[position].about)
+                dialogCustom.timeMovie.setText(array[position].time)
 
+                val dialog = AlertDialog.Builder(this@MainActivity,0)
+                    .setTitle("Edit Movie")
+                    .setView(dialogCustom.root)
+                    .create()
+                dialogCustom.saveMovie.setOnClickListener{
+                    val name = dialogCustom.nameMovie.text.toString()
+                    val about = dialogCustom.aboutMovei.text.toString()
+                    val time = dialogCustom.timeMovie.text.toString()
+                    if (name.isEmpty() || about.isEmpty() || time.isEmpty()){
+                        Toast.makeText(this@MainActivity,"Ma'lumotlar kiritilmadi",Toast.LENGTH_SHORT).show()
+                        return@setOnClickListener
+                    }
+                    val malumot = Malumot(name,about,time)
+                    array[position]=malumot
+                    customAdapter.notifyItemChanged(position)
+                    dialog.dismiss()
+                }
+                dialog.show()
             }
             override fun removeItem(position: Int) {
                 array.removeAt(position)
@@ -42,8 +60,27 @@ class MainActivity : AppCompatActivity() {
         })
 
         binding.addMovie.setOnClickListener {
-            val intents = Intent(this, AddMovie::class.java)
-            startActivity(intents)
+            val dialogCustom = ActivityAddMovieBinding.inflate(layoutInflater,null,false)
+
+            val dialog = AlertDialog.Builder(this,0)
+                .setTitle("Add Movie")
+                .setView(dialogCustom.root)
+                .create()
+                    dialogCustom.saveMovie.setOnClickListener{
+                        val name = dialogCustom.nameMovie.text.toString()
+                        val about = dialogCustom.aboutMovei.text.toString()
+                        val time = dialogCustom.timeMovie.text.toString()
+                        if (name.isEmpty() || about.isEmpty() || time.isEmpty()){
+                            Toast.makeText(this,"Ma'lumotlar kiritilmadi",Toast.LENGTH_SHORT).show()
+                            return@setOnClickListener
+                        }
+                        val malumot = Malumot(name,about,time)
+                        array.add(malumot)
+                        customAdapter.notifyItemInserted(array.size-1)
+                        dialog.dismiss()
+                    }
+
+            dialog.show()
         }
     }
 
